4206c206b57bf81156c03935d2885eb838e1406f
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-12-08 04:59:38 +0000

    adding attribute accessors to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,14 @@
 #  photo_id   :integer
 #
 class Comment < ApplicationRecord
+
+  #Validations:
+  validates :photo_id, presence: true
+  validates :body, presence: true
+  validates :author_id, presence: true
+
+  #Direct Associations:
+  belongs_to :commenter, required: true, class_name: "User", foreign_key: "author_id"
+  belongs_to :photo, required: true, class_name: "Photo", foreign_key: "photo_id", counter_cache: true
+
 end
