3140df36a7446addc2abd3f723b4ad4aa52ebe97
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-12-08 19:47:24 +0000

    user show v2

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,8 +36,8 @@ validates :username, uniqueness: true
 #Direct Associations
 has_many  :likes, class_name: "Like", foreign_key: "fan_id", dependent: :destroy
 has_many  :comments, class_name: "Comment", foreign_key: "author_id", dependent: :destroy
-has_many  :sent_follow_requests, class_name: "Followrequest", foreign_key: "sender_id", dependent: :destroy
-has_many  :received_follow_requests, class_name: "Followrequest", foreign_key: "recipient_id", dependent: :destroy
+has_many  :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id", dependent: :destroy
+has_many  :received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id", dependent: :destroy
 has_many  :own_photos, class_name: "Photo", foreign_key: "owner_id", dependent: :destroy
 
 #Indirect Associations
