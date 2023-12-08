d8aba8cf28dcff28631b62335524aedd048396b7
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-12-08 05:10:07 +0000

    adding list of users

--- config/routes.rb
+++ config/routes.rb
@@ -75,9 +75,10 @@ Rails.application.routes.draw do
 
   #------------------------------
 
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #Routes for Users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+
+  get("/", controller: "users", action: "index")
+
 end
