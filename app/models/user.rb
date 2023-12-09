23b1cbc3e8e69d589ccfa4add9b812e3a7aa0b56
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-12-09 21:42:06 +0000

    adding accepted follow request association scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,27 +24,54 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
 
-#Validations
-validates :username, presence: true
-validates :username, uniqueness: true
+  #Validations
+  validates :username, presence: true
+  validates :username, uniqueness: true
 
-#Direct Associations
-has_many  :likes, class_name: "Like", foreign_key: "fan_id", dependent: :destroy
-has_many  :comments, class_name: "Comment", foreign_key: "author_id", dependent: :destroy
-has_many  :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id", dependent: :destroy
-has_many  :received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id", dependent: :destroy
-has_many  :own_photos, class_name: "Photo", foreign_key: "owner_id", dependent: :destroy
+  #Direct Associations
+  has_many  :likes, class_name: "Like", foreign_key: "fan_id", dependent: :destroy
+  has_many  :comments, class_name: "Comment", foreign_key: "author_id", dependent: :destroy
+  has_many  :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id", dependent: :destroy
+  #has_many  :accepted_sent_follow_requests => {accepted}, class_name: "FollowRequest", foreign_key: "sender_id", dependent: :destroy
+  has_many :accepted_sent_follow_requests, -> { where({ :status => 'accepted' }) }, :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy 
 
-#Indirect Associations
-has_many :following, through: :sent_follow_requests, source: :recipient
-has_many :followers, through: :received_follow_requests, source: :sender
-has_many :liked_photos, through: :likes, source: :photo
-has_many :feed, through: :following, source: :own_photos
-has_many :activity, through: :following, source: :liked_photos
-  
+  has_many  :received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id", dependent: :destroy
+  has_many  :own_photos, class_name: "Photo", foreign_key: "owner_id", dependent: :destroy
+
+  #Indirect Associations
+  has_many :following, through: :accepted_sent_follow_requests, source: :recipient
+  has_many :followers, through: :received_follow_requests, source: :sender
+  has_many :liked_photos, through: :likes, source: :photo
+  has_many :feed, through: :following, source: :own_photos
+  has_many :activity, through: :following, source: :liked_photos
+
+  #Instance Methods
+
+  # def accepted_followers
+
+  #   accepted = self.sent_follow_requests.where(sender_id: self.id, status: "accepted")
+
+  #   accepted_feed = []
+
+  #   accepted.each do |a_follower|
+
+  #     feed_photo = Photo.where(owner_id: a_follower.id)
+
+  #   return accepted
+
+  # end
+
+  # def accepted_followers_feed
+
+  #   accepted_feed = Photo.where(owner_id: self.accepted_followers)
+
+  #   return accepted_feed
+
+  # end
+    
 end
